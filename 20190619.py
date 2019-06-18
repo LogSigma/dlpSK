fe149b9988e43d8f7ad0a862d7edcbc947f0e20b
Ji Su Lee <32809393+LogSigma@users.noreply.github.com>
2019-06-19 02:02:57 +0900

    Update 20190619.py

--- 20190619.py
+++ 20190619.py
@@ -19,20 +19,23 @@ df_m1 = df_m1.T.fillna(df_m1.mean(axis=1)).T
 
 df_m1_y = df_m1['Y']
 
-df_train = df_m1.sample(frac=0.7, random_state=0, replace=False)
+#df_train = df_m1.sample(frac=0.7, random_state=0, replace=False)
+df_train = df_m1[:9430]
 df_test = df_m1.drop(df_train.index.tolist())
+
 
 df_train_y = df_train['Y']
 df_test_y = df_test['Y']
 
 df_train_x = df_train.drop(['Y','END_TM','Validation'], axis=1)
-df_test_x=df_test.drop(['Y','END_TM','Validation'],axis=1)
+df_test_x = df_test.drop(['Y','END_TM','Validation'],axis=1)
 
-##MODEL
-gmodel = GradientBoostingRegressor(n_estimators = 500, random_state=0, max_depth =5)
-gfit= gmodel.fit(df_train_x, df_train_y)
-g_df_test_pre = pd.DataFrame(gfit.predict(df_test_x))
+#%%
+model = GradientBoostingRegressor(n_estimators = 500, random_state=0, max_depth =5)
+fit = model.fit(df_train_x, df_train_y)
+g_df_test_pre = pd.DataFrame(fit.predict(df_test_x))
 print('R2: ',r2_score(df_test_y, g_df_test_pre))
 print('MSE: ',mean_squared_error(df_test_y, g_df_test_pre))
+
 ###R2:  0.6236543671937749
 ###MSE:  18.32816473236871
