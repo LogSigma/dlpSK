d797f097194c94503d9ed136a9699967949a652c
Ji Su Lee <32809393+LogSigma@users.noreply.github.com>
2019-06-19 01:55:14 +0900

    Create 20190619.py

---
+++ 20190619.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.metrics import r2_score, mean_squared_error
+
+df = pd.read_csv('D:/DataSet_accuracy.csv', sep=",")
+
+df_t = df
+df_t['END_TM'] = pd.to_datetime(df_t['END_TM'], format='%Y-%m-%d %p %H:%M:%S')
+
+df_t['END_TM_INT'] = pd.to_numeric(df_t['END_TM']-df_t['END_TM'][4])/1000000000
+
+df_t = df_t.set_index('ID')
+
+df_t = pd.get_dummies(df_t, columns=['A','B'])
+
+df_m1 = df_t[df_t['Validation']==0]
+
+df_m1 = df_m1.T.fillna(df_m1.mean(axis=1)).T
+
+df_m1_y = df_m1['Y']
+
+df_train = df_m1.sample(frac=0.7, random_state=0, replace=False)
+df_test = df_m1.drop(df_train.index.tolist())
+
+df_train_y = df_train['Y']
+df_test_y = df_test['Y']
+
+df_train_x = df_train.drop(['Y','END_TM','Validation'], axis=1)
+df_test_x=df_test.drop(['Y','END_TM','Validation'],axis=1)
+
+##MODEL
+gmodel = GradientBoostingRegressor(n_estimators = 500, random_state=0, max_depth =5)
+gfit= gmodel.fit(df_train_x, df_train_y)
+g_df_test_pre = pd.DataFrame(gfit.predict(df_test_x))
+print('R2: ',r2_score(df_test_y, g_df_test_pre))
+print('MSE: ',mean_squared_error(df_test_y, g_df_test_pre))
